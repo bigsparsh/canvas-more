dfd48941d406b1fe4563b0d3f1d17ecd087d8e32
Sparsh Singh <sparshsingh7586@gmail.com>
2023-12-21 15:12:08 +0530

    Implemented basic point class

---
+++ Point.js
@@ -0,0 +1,18 @@
+class Point {
+	constructor(x, y, ctx, color = "white", radius = 5) {
+		this.x = x;
+		this.y = y;
+		this.ctx = ctx;
+		this.color = color;
+		this.radius = radius;
+	}
+	draw() {
+		this.ctx.beginPath();
+		this.ctx.save();
+		this.ctx.fillStyle = this.color;
+		this.ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2);
+		this.fill();
+		this.ctx.restore();
+		this.ctx.closePath();
+	}
+}
